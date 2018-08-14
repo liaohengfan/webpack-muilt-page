e1ff2ed06ada958758fc8aa2a70d036ae7c7acc5
liaohengfan <liaohengfan@yeah.net>
2018-08-14 17:07:16 +0800

    delete external libs

--- config/config.js
+++ config/config.js
@@ -1,8 +1,12 @@
 module.exports={
-    dev_host:"localhost",
-    dev_port:8090,
-    favicon:'./src/images/favicon.ico',
-    commonname:"common",
+    dev_host: "192.168.40.209",
+    dev_port: 8090,
+    favicon: './src/images/favicon.ico',
+    commonname: "common",
+    cssOutPath:"./css",
+    cssPublicPath:"../",
+    fontsOutPath:"./fonts",
+    imageOutPath:"./img",
     entrys:[
         {
             title:"主页",
